d18817d24cad35f01a47ffb6813d2aef8becd3fc
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2024-11-08 20:46:50 +0100

    add curve graph image

--- flake.nix
+++ flake.nix
@@ -15,14 +15,22 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
+          inherit (pkgs.texlive) scheme-small latex-bin latexmk beamer polski;
         };
       in
       rec {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+            texlab            
+          ];
+          shellHook = ''
+            fish -C "source ~/.config/home-manager/venv.fish" -i && exit
+          '';
+        };
         packages = {
           document = pkgs.stdenvNoCC.mkDerivation rec {
-            name = "latex-demo-document";
-            src = self;
+            name = "latex-beamer-document";
+            src = ./.;
             buildInputs = [
               pkgs.coreutils
               tex
@@ -35,11 +43,14 @@
             buildPhase = ''
               export PATH="${pkgs.lib.makeBinPath buildInputs}";
               mkdir -p .cache/texmf-var
+              rm -f ${name}.pdf
               env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
                   SOURCE_DATE_EPOCH=${toString self.lastModified} \
                   latexmk -interaction=nonstopmode -pdf -lualatex \
                   -pretex="\pdfvariable suppressoptionalinfo 512\relax" \
-                  -usepretex doc.tex
+                  -usepretex \
+                  -shell-escape \
+                  ${name}.tex
             '';
             installPhase = ''
               mkdir -p $out
